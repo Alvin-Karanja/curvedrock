e8dcfe1ab39908a25e6a8953bae183252e787cd7
Alvin-Karanja <alvin2@proton.me>
2023-08-18 20:32:57 +0300

    handling 404s

--- src/Products.jsx
+++ src/Products.jsx
@@ -2,6 +2,7 @@ import React, {useState} from "react";
 import Spinner from "./Spinner";
 import useFetch from "./services/useFetch";
 import {useParams} from "react-router-dom";
+import PageNotFound from "./PageNotFound";
 
 export default function Products() {
     const [size, setSize] = useState("");
@@ -30,8 +31,8 @@ export default function Products() {
         : products;
 
     if (error) throw error;
-
     if (loading) return <Spinner/>;
+    if (products.length === 0) return <PageNotFound/>
 
     return (
         <>
