981ef51689628995bc02f5e613329589cea4241f
Alvin-Karanja <alvin2@proton.me>
2023-08-22 18:53:48 +0300

    tracking touch for instant validation

--- src/Checkout.jsx
+++ src/Checkout.jsx
@@ -18,6 +18,7 @@ export default function Checkout({ cart, emptyCart }) {
   const [address, setAddress] = useState(emptyAddress);
   const [status, setStatus] = useState(STATUS.IDLE);
   const [saveError, setSaveError] = useState(null);
+  const [touched, setTouched] = useState({});
 
   // Derived State
   const errors = getErrors(address);
@@ -34,7 +35,9 @@ export default function Checkout({ cart, emptyCart }) {
   }
 
   function handleBlur(event) {
-    // TODO
+    setTouched((cur) => {
+      return { ...cur, [event.target.id]: true };
+    });
   }
 
   async function handleSubmit(event) {
@@ -89,6 +92,9 @@ export default function Checkout({ cart, emptyCart }) {
             onBlur={handleBlur}
             onChange={handleChange}
           />
+          <p role="alert">
+            {(touched.city || status === STATUS.SUBMITTED) && errors.city}
+          </p>
         </div>
 
         <div>
@@ -106,6 +112,9 @@ export default function Checkout({ cart, emptyCart }) {
             <option value="United Kingdom">United Kingdom</option>
             <option value="USA">USA</option>
           </select>
+          <p role="alert">
+            {(touched.country || status === STATUS.SUBMITTED) && errors.country}
+          </p>
         </div>
 
         <div>
