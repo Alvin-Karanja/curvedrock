27552c1ad1b30f56433bb237efa416f6851a7e8a
Alvin-Karanja <alvin2@proton.me>
2023-08-22 16:41:52 +0300

    implement immutable-friendly delete

--- src/App.jsx
+++ src/App.jsx
@@ -27,7 +27,9 @@ export default function App() {
 
   function updateQuantity(sku, quantity) {
     setCart((items) => {
-      return items.map( (i) => i.sku === sku ? { ...i, quantity } : i);
+      return quantity === 0
+        ? items.filter((i) => i.sku !== sku)
+        : items.map((i) => (i.sku === sku ? { ...i, quantity } : i));
     });
   }
 
@@ -43,7 +45,10 @@ export default function App() {
               path="/:category/:id"
               element={<Detail addToCart={addToCart} />}
             />
-            <Route path="/cart" element={<Cart cart={cart} updateQuantity={updateQuantity} />} />
+            <Route
+              path="/cart"
+              element={<Cart cart={cart} updateQuantity={updateQuantity} />}
+            />
           </Routes>
         </main>
       </div>
