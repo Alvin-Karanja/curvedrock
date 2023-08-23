3add4b3847b04f3284680a6328b35803b4bcc249
Alvin-Karanja <alvin2@proton.me>
2023-08-23 13:33:08 +0300

    Handling Unexpected Action Type

--- src/cartReducer.js
+++ src/cartReducer.js
@@ -2,5 +2,7 @@ export default function cartReducer(cart, action) {
     switch (action.type) {
         case "empty":
             return [];
+        default:
+            throw new Error("Unhandled action" + action.type);
     }
 }
