0e81c0178ffe9c02e64ef182daebd8fd611823b9
Alvin-Karanja <alvin2@proton.me>
2023-08-23 15:05:04 +0300

    creating a reuseable useContext Hook

--- src/cartContext.js
+++ src/cartContext.js
@@ -1,27 +1,31 @@
-import React, { useEffect, useReducer } from "react";
-import cartReducer from "./cartReducer"
+import React, { useContext, useEffect, useReducer } from "react";
+import cartReducer from "./cartReducer";
 
-export const CartContext = React.createContext(null);
+const CartContext = React.createContext(null);
 
 let initialCart;
 try {
-    initialCart = JSON.parse(localStorage.getItem("cart")) ?? [];
+  initialCart = JSON.parse(localStorage.getItem("cart")) ?? [];
 } catch {
-    console.error("The cart could not be parsed into JSON.");
-    initialCart = [];
+  console.error("The cart could not be parsed into JSON.");
+  initialCart = [];
 }
 
-
 export function CartProvider(props) {
-    const [cart, dispatch] = useReducer(cartReducer, initialCart);
-    useEffect(() => localStorage.setItem("cart", JSON.stringify(cart)), [cart]);
-    const contextValue = {
-        cart,
-        dispatch,
-    };
-    return (
-        <CartContext.Provider value={contextValue}>
-            {props.children}
-        </CartContext.Provider>
-    );
+  const [cart, dispatch] = useReducer(cartReducer, initialCart);
+  useEffect(() => localStorage.setItem("cart", JSON.stringify(cart)), [cart]);
+  const contextValue = {
+    cart,
+    dispatch,
+  };
+  return (
+    <CartContext.Provider value={contextValue}>
+      {props.children}
+    </CartContext.Provider>
+  );
+}
+
+export function useCart() {
+  const context = useContext(CartContext);
+  return context;
 }
